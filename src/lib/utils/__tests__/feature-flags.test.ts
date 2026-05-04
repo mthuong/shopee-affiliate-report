d6dbc9bd15720caf38f930a56d9ce003970b503b
Tom Nguyen <mttbit@gmail.com>
2026-05-04 23:43:17 +0700

    feat: add react-image-crop dep and CROP_CONFIRM_ENABLED flag

---
+++ src/lib/utils/__tests__/feature-flags.test.ts
@@ -0,0 +1,30 @@
+describe('CROP_CONFIRM_ENABLED', () => {
+  const ORIGINAL = process.env.NEXT_PUBLIC_ENABLE_CROP_CONFIRM
+
+  afterEach(() => {
+    if (ORIGINAL === undefined) delete process.env.NEXT_PUBLIC_ENABLE_CROP_CONFIRM
+    else process.env.NEXT_PUBLIC_ENABLE_CROP_CONFIRM = ORIGINAL
+    jest.resetModules()
+  })
+
+  it('defaults to true when env var is unset', () => {
+    delete process.env.NEXT_PUBLIC_ENABLE_CROP_CONFIRM
+    jest.resetModules()
+    const { CROP_CONFIRM_ENABLED } = require('../feature-flags')
+    expect(CROP_CONFIRM_ENABLED).toBe(true)
+  })
+
+  it('is true when env var is "true"', () => {
+    process.env.NEXT_PUBLIC_ENABLE_CROP_CONFIRM = 'true'
+    jest.resetModules()
+    const { CROP_CONFIRM_ENABLED } = require('../feature-flags')
+    expect(CROP_CONFIRM_ENABLED).toBe(true)
+  })
+
+  it('is false when env var is exactly "false"', () => {
+    process.env.NEXT_PUBLIC_ENABLE_CROP_CONFIRM = 'false'
+    jest.resetModules()
+    const { CROP_CONFIRM_ENABLED } = require('../feature-flags')
+    expect(CROP_CONFIRM_ENABLED).toBe(false)
+  })
+})
